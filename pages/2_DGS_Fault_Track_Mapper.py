4547431622741c8275584f732dcb75db3863bd7e
Arcee <arceetaraguajuan@gmail.com>
2026-01-27 09:21:51 +0800

    updated files

--- pages/2_DGS_Fault_Track_Mapper.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="DGS Fault & Track Mapper", layout="wide")
-st.title("DGS Fault & Track Mapper")
-st.info("Placeholder. Upload/point me to your Fault & Track Mapper Streamlit script, then I’ll wire it here.")
-st.write("Back to portal:")
-if st.button("⬅ Home"):
-    st.switch_page("app.py")
